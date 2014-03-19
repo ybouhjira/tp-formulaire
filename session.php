5183ed4d92856681d09f89668996e898a327079e
ybouhjira <b.youssef91@gmail.com>
2014-03-19 15:21:54 +0000

    Removed useless comments

--- session.php
+++ session.php
@@ -6,9 +6,6 @@ try
         throw new Exception("Impossible de lancer une session");
 
     $_SESSION = array_merge($_SESSION, $_POST);
-
-    // foreach ($_POST as $key => $value)
-    //     $_SESSION[$key] = $value;
 }
 catch(Exception $exc)
 {
